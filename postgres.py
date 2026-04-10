1fc8313bbb79c1df387b017e9671f7e6715cd303
Prajwal <karkiprajwal873@gmail.com>
2026-04-10 13:00:59 +0545

    dataclass

--- postgres.py
+++ postgres.py
@@ -13,8 +13,8 @@ class User(Base):
     def __repr__(self):
         return f"User(id={self.id}, name='{self.name}', age={self.age})"
 
-engine = create_engine('sqlite:///users.db')
-Base.metadata.create_all(engine)
+engine = create_engine('sqlite:///users.db', echo=True) #database connection
+Base.metadata.create_all(engine) #database migration
 
 with Session(engine) as session:
     new_user = User(name='Alice', age=30)
